98491e915b2aa7e11d6ee0245c22ceb5ac4081d7
Raghav Sethi <rsraghavsethi@gmail.com>
2018-10-09 14:21:33 +0530

    Page 1 Layout Complete

--- Vizag Fintech Festival/ViewController.swift
+++ Vizag Fintech Festival/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func ConferenceButton(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func conferenceButton(conference : UIButton)    {
+            
+    }
 
 }
 
